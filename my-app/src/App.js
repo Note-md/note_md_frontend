ae2e363283bcdd8854be949d59c4c2e7f75ac246
MuhammadElsoukkary <119015633+MuhammadElsoukkary@users.noreply.github.com>
2023-10-22 12:54:17 -0400

    Initial Commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Profile from './Views/Pages/Profile'
+import Contact from './Views/Pages/Contact';
+import Navigation from './Navigation';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={Profile} />
+          <Route path="/contact" component={Contact} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
