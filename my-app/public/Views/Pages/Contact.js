605d7bd185b7935bac96d68f45c3632d7b1fbab7
MuhammadElsoukkary <119015633+MuhammadElsoukkary@users.noreply.github.com>
2023-10-22 17:14:14 -0400

    Small Change

--- my-app/public/Views/Pages/Contact.js
+++ my-app/public/Views/Pages/Contact.js
@@ -1,25 +1,24 @@
 import React from 'react';
+import Footer from '../Pages/'
+import Header from '../../Shared/Header.Js';
+import ProfileBtn from  ''
+import BoardBtn from '../Buttons/BoardBtn';
+import '../Css/Contact';
 
 const Contact = () => {
   return (
+    
     <div>
-      <h2>Contact Page</h2>
-      <p>Contact us for more information!</p>
-      <button></button>
+      <ProfileBtn></ProfileBtn>
+      <Header></Header>
+      <h1>Contact us for more info</h1>
+      <p>Leave us a review here </p>
+      <BoardBtn></BoardBtn>
+      include("../shared/footer")
+      <Footer />
 
 
-
-
-
-
-    <footer>copyright Note_MD</footer>
     </div>
-
-    
-
-    
-
-    
 
     
   );
