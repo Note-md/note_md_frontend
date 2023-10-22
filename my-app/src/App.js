3f1cfc0f77893ec11da134e228167faaad032f55
MuhammadElsoukkary <119015633+MuhammadElsoukkary@users.noreply.github.com>
2023-10-22 17:54:41 -0400

    Fixed errors

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom'; // Import Switch directly from react-router-dom
-import Profile from './Views/Pages/Profile';
-import Contact from './Views/Pages/Contact';
-import Board from './Views/Pages/Board.Js';
-import Home from './Views/Pages/Home.Js';
+import Profile from './/Views/Pages/Home';
+import Contact from './/Views/Pages/Contact.Js';
+import Board from './/Views/Pages/Board.Js';
+import Home from './/Views/Pages/Home.js';
 
 function App() {
   return (
